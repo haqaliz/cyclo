60655ba2ebe971b5062f566ee903256fafc64f93
Ali Haqiqi <aliz@foresightanalytics.ca>
2023-09-25 16:05:10 +0330

    make insight page fully accessible for now
    Signed-off-by: Ali Haqiqi <aliz@foresightanalytics.ca>

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -7,6 +7,22 @@ const user = writable(null);
 user.get = async () => {
     const r = await usr.info();
     if (!r) return;
+    if (!r.subscription) r.subscription = {
+        "updated_at": {
+            "seconds": 1694285050,
+            "nanoseconds": 397000000
+        },
+        "user_id": r.id,
+        "expired_at": null,
+        "product_id": "prod_OF8wKOVKrYa9Bv",
+        "price_id": "price_1NSeetH3ShbgzS1OC9jNUugA",
+        "created_at": {
+            "seconds": 1693389554,
+            "nanoseconds": 758000000
+        },
+        "subscription_id": "sub_1NYrflH3ShbgzS1O6OOSMOtu",
+        "key": "beginner_monthly"
+    }
     user.set(r);
     await plans.get();
 };
