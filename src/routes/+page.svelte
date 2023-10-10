b67b1b8e9938b8ef8e06395227f7a61005b6ad69
Ali Haqiqi <aliz@foresightanalytics.ca>
2023-10-10 13:58:15 +0330

    seo main page, login and signup pages
    Signed-off-by: Ali Haqiqi <aliz@foresightanalytics.ca>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,36 @@
 <script lang="ts">
 	import { user } from '$stores';
+	const origin = {
+		'development:local': 'https://cyclotic-fc537.web.app/',
+		development: 'https://cyclotic-fc537.web.app/',
+		staging: 'https://cyclotic-fc537.web.app/',
+		production: 'https://cyclo.dev/'
+	}[import.meta.env.MODE];
+	const title = 'Cyclo';
+	const description =
+		'Empower your menstrual health journey with our comprehensive tracking and insights app. Join us today!';
 </script>
 
-<!-- Root Placeholder -->
 <svelte:head>
-	<title>Cyclo</title>
-	<meta
-		name="description"
-		content="Empower your menstrual health journey with our comprehensive tracking and insights app. Join us today!"
-	/>
+	<!-- Primary Meta Tags -->
+	<title>{title}</title>
+	<meta name="title" content={title} />
+	<meta name="description" content={description} />
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content={origin} />
+	<meta property="og:title" content={title} />
+	<meta property="og:description" content={description} />
+	<meta property="og:image" content={`${origin}img/misc/cover.webp`} />
+
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:url" content={origin} />
+	<meta property="twitter:title" content={title} />
+	<meta property="twitter:description" content={description} />
+	<meta property="twitter:image" content={`${origin}img/misc/cover.webp`} />
+
 	<style>
 		body {
 			background-color: #e9d5ff;
