beebc79a3480fdddf69ce91caad1aa4f7a730821
Ali Haqiqi <aliz@foresightanalytics.ca>
2023-10-20 22:24:22 +0330

    fx responsive issue
    Signed-off-by: Ali Haqiqi <aliz@foresightanalytics.ca>

--- src/lib/components/recommendations.svelte
+++ src/lib/components/recommendations.svelte
@@ -16,26 +16,26 @@
 
 {#if menstruationProduct}
 	<div
-		class="flex flex-row items-start transition-colors rounded p-2 sm:p-4
+		class="flex flex-col sm:flex-row items-start transition-colors rounded p-2 sm:p-4
             min-w-[280px] sm:min-w-[250px] min-h-[180px] sm:min-h-[120px]
             bg-gray-100 hover:bg-gray-200"
 	>
-		<div class={`flex flex-col ${menstruationProductColor} rounded mr-2 sm:mr-4`}>
+		<div class={`flex flex-col items-center ${menstruationProductColor} rounded mr-2 sm:mr-4 mb-2 sm:mb-0 w-full sm:w-auto`}>
 			<div
 				class="bg-no-repeat bg-contain bg-center w-[240px] h-[240px] mix-blend-multiply"
 				style:background-image={`url(${menstruationProduct?.img ?? ''})`}
 			/>
 		</div>
 		<div class="flex flex-col items-start">
-			<div class="flex flex-row items-start">
+			<div class="flex flex-col sm:flex-row w-full sm:w-auto items-start mb-2 sm:mb-0">
 				<h3
-					class={`font-semibold text-3xl ${menstruationProductColor} p-2 sm:p-4 rounded mb-2 sm:mb-4 mr-2 sm:mr-4`}
+					class={`font-semibold w-full sm:w-auto text-3xl ${menstruationProductColor} p-2 sm:p-4 rounded mb-2 sm:mb-4 mr-2 sm:mr-4`}
 				>
 					<span>$</span>
 					<span>{menstruationProduct?.price?.value}</span>
 				</h3>
 				<a
-					class="p-2 sm:p-4 rounded font-sans font-medium focus:outline-none focus:ring-2
+					class="p-2 sm:p-4 w-full sm:w-auto rounded font-sans font-medium focus:outline-none focus:ring-2
                     focus:ring-opacity-75 ease-in-out duration-300 flex flex-row items-center
                     bg-zinc-900 text-white hover:bg-gray-700 focus:ring-gray-400 justify-center
                     "
